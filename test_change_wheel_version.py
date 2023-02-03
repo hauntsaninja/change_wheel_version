9eeef415d1f88e36d6272d3721af717c09328cb9
hauntsaninja <hauntsaninja@gmail.com>
2023-02-02 19:33:11 -0800

    fix installer test

--- test_change_wheel_version.py
+++ test_change_wheel_version.py
@@ -13,6 +13,7 @@ def get_installed_version(python: Path, dist: str = "pypyp") -> bytes:
     return subprocess.check_output([python, "-c", prog]).strip()
 
 
+PYP_DIST = "pypyp"
 PYP_V1_WHEEL = "https://files.pythonhosted.org/packages/ad/c2/92fa4ab416c7f697a2944d54c6e58ed5d043e296f6f671af32aaacb4b40e/pypyp-1.0.0-py3-none-any.whl"  # noqa: E501
 
 
@@ -81,9 +82,11 @@ def test_change_wheel_version_installer() -> None:
         subprocess.check_call([python, "-m", "installer", changed_wheel])
         assert get_installed_version(python) == b"1.0.0+yikes"
 
+        subprocess.check_call([pip, "uninstall", "-y", PYP_DIST])
+
         # change the wheel but with a long version
         version = "super.long.version.string.that.is.longer.than.sixty.eight.characters.even.eighty"
         assert len(version) == 80
         changed_wheel = change_wheel_version.change_wheel_version(original_wheel, "1", version)
-        subprocess.check_call([pip, "install", changed_wheel])
+        subprocess.check_call([python, "-m", "installer", changed_wheel])
         assert get_installed_version(python) == b"1+" + version.encode("utf-8")
